f516233bf94f423c466eb332530b7b902ed2c3c0
nonyedev1 <dachbookings@gmail.com>
2023-10-08 19:44:35 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const arrowUp = document.querySelectorAll('.arrowUp');
+const arrowDown = document.querySelectorAll('.arrowDown');
+const toDisplay = document.querySelectorAll('.toDisplay');
+
+for(let i=0; i<arrowDown.length; i++){
+  arrowDown[i].addEventListener('click', ()=>{
+    arrowDown[i].classList.toggle('hide')
+    arrowUp[i].classList.toggle('hide')
+    toDisplay[i].classList.add('hide');
+  
+  })
+}
+for(let i=0; i<arrowUp.length; i++){
+  arrowUp[i].addEventListener('click', ()=>{
+    arrowUp[i].classList.toggle('hide')
+    arrowDown[i].classList.toggle('hide')
+    toDisplay[i].classList.remove('hide');
+  })
+}
+
